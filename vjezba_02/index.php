04a0e133f9033a44af87616d13021efd7b640b11
bbarac1 <bbarac1@vvg.hr>
2025-01-29 19:49:51 +0100

    Create index.php

---
+++ vjezba_02/index.php
@@ -0,0 +1,30 @@
+<?php
+$title = "Da Vincijev kod";
+$link = "https://hr.wikipedia.org/wiki/Da_Vincijev_kod";
+ 
+print '
+<!DOCTYPE html>
+    <head>
+        <title>Da Vincijev kod</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="Bruno Barac">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <h1>';
+        print $title;
+        print'</h1>
+        <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.<br>
+        <a href="';
+        print $link;
+        print '" target="_blank">';
+        print $link; 
+        print '</a></p>
+    </body>
+    </html>
+    <!--vjezba_2.php -->'
+?> 
+  
